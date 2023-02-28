d14fdb07e1bc65e26744d4a0db7c7556c5d6bf2c
melon musk <125475444+JoaomBRosio@users.noreply.github.com>
2023-02-28 16:52:23 -0300

    Add files via upload

---
+++ aula_07/index.php
@@ -0,0 +1,25 @@
+<?php
+
+//concatenação
+
+$frase = ' Meu nome é ';
+$nome= 'Joao'; 
+$idade= 16;
+$nomeclasse = 'box'; 
+
+define('NOME','João');
+
+//echo 'Meu nome é '.NOME. ' e tenho '.$idade. ' anos';
+
+//echo 'Olá meu nome é'.$nome;
+//echo "Meu nome é $nome"; 
+
+
+//echo "Olá, meu nome é $nome e tenho $idade anos";
+
+
+echo "<div class=\"$nomeclasse\"> Meu conteudo da div </div> ";
+
+
+
+?>
